2977cbf1c86d7a18e4524d4a44b3aa480cde5990
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-06-28 11:04:17 +0300

    practice-api-springboot

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -44,6 +44,11 @@ public class FakePersonDataAccessService implements PersonDao {
 
     @Override
     public int deletePersonById(UUID id) {
-        return 0;
+        Optional <Person> personMaybe = selectPersonById(id);
+        if(personMaybe.isEmpty()){
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
     }
 }
