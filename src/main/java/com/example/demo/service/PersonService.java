39cbb4cb57d3df2c37a994f12fb758c7e6fd4889
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-06-28 08:52:30 +0300

    practice-api-springboot

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -12,7 +12,7 @@ public class PersonService {
     //get a reference of the actual person DAO
 
     private final PersonDao personDao;
-
+    // the qualifier acts like a tag, as the PersonDao interface could be implemented by multiple classes, the qualifier helps to specify which class used it.
     @Autowired
     public PersonService(@Qualifier("fakeDAO") PersonDao personDao) {
         this.personDao = personDao;
